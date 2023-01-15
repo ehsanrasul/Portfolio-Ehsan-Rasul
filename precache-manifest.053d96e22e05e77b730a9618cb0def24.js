e0ae21db568f2ed474d395dd9aa7c91f193a82b7
Ehsan Rasul <ehsanrasul0@gmail.com>
2023-01-15 12:32:41 +0500

    Updates

---
+++ precache-manifest.053d96e22e05e77b730a9618cb0def24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2544ee1188f0c3040816",
+    "url": "/Portfolio-Ehsan-Rasul/static/css/main.85da6f6a.chunk.css"
+  },
+  {
+    "revision": "2544ee1188f0c3040816",
+    "url": "/Portfolio-Ehsan-Rasul/static/js/main.2544ee11.chunk.js"
+  },
+  {
+    "revision": "23e5026d3c124a934bc7",
+    "url": "/Portfolio-Ehsan-Rasul/static/js/1.23e5026d.chunk.js"
+  },
+  {
+    "revision": "68f5cbc8129f7c0ab1ba",
+    "url": "/Portfolio-Ehsan-Rasul/static/js/2.68f5cbc8.chunk.js"
+  },
+  {
+    "revision": "56bae67a7ceb80fe50d2",
+    "url": "/Portfolio-Ehsan-Rasul/static/js/runtime~main.56bae67a.js"
+  },
+  {
+    "revision": "e09b180dc8319f456e5a6dc2ce19262c",
+    "url": "/Portfolio-Ehsan-Rasul/index.html"
+  }
+];
